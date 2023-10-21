20ea64719518dff87f3d4286698f73d64b60979c
rashika-veera <rashikaveera@gmail.com>
2023-10-21 15:26:16 +0530

    Added Search func

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -6,7 +6,9 @@ import resList from "../utils/mockData";
 const ResContainer = () =>{
     const [listofrestaurants,setlistofrestaurants]=useState([]);
     const [filtereddata,setfiltereddata]=useState([]);
-    const [toggle,settoggle]=useState(false)
+    const [types,settypes]=useState('');
+    const [toggle,settoggle]=useState(true)
+    const [filterbtn,setfilterbtn]=useState('filterbtn')
     useEffect( ()=>{
         fetchdata();
     },[])
@@ -17,24 +19,40 @@ const ResContainer = () =>{
         json_data=await data_from_api.json();
         setlistofrestaurants(json_data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setfiltereddata(json_data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-        console.log(listofrestaurants)
-        console.log(filtereddata)
     }
 
     return  listofrestaurants.length===0? (<Shimmer/>): (
         <>
-        <button className="filter-btn" onClick={()=>{
+        <button className={filterbtn} onClick={()=>{
             settoggle(!toggle)
             if (toggle){
                 const filtereddata=listofrestaurants.filter((res)=> res.info.avgRating > 4.0);
                 setfiltereddata(filtereddata); 
+            setfilterbtn('apply-filterbtn')
             }
             else{
                 setfiltereddata(listofrestaurants); 
+                setfilterbtn('filterbtn')
             }
             
         }}> Top Rated Restaurants</button>
-        
+
+
+<input type="text" className="searchs" value={types} onChange={(e)=>{
+                    settypes(e.target.value)
+                }} placeholder="search"/> 
+
+        <button className="search-filter" onClick={
+            ()=>{
+          
+               const searchfilter=listofrestaurants.filter( (restaurants)=>(
+                restaurants.info.name.toLowerCase().includes(types.toLowerCase())
+                
+            ))
+            setfiltereddata(searchfilter);
+            settoggle(!toggle)
+            }
+        }> Search</button>
         
         <div className="res-container">
                   { filtereddata.map( (restaurant) => (
